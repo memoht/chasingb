9279336b6f58b40096d726fa4226d9ac1308d61d
guillermo haas-thompson <aesthetica@me.com>
2012-09-23 23:38:00 -0400

    initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chasingb::Application.config.secret_token = '61d8234c2736307508e8b402d0d4d4a8d68e095aac91312d9ca9137acce1ed648c7e2adf85de348b0fb76c3e8d5af339f7fcf14cc824e0fff6b13f60a5123b49'
