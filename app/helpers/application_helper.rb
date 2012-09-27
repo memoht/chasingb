ff6c92b4bbcb1577489b21ad2dedcdf21159945b
guillermo haas-thompson <aesthetica@me.com>
2012-09-27 16:58:20 -0400

    markdown helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,9 @@ module ApplicationHelper
     %|#{controller.controller_name}-#{controller.action_name}|
   end
 
+  def markdown
+    Redcarpet::Markdown.new(Redcarpet::Render::HTML.new,
+      {:autolink => true, :space_after_headers => true, :hard_wrap => true, :safe_links_only => true})
+  end
+
 end
