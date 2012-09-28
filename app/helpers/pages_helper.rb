1657b8ad768ff827fd72cbb9a6134c0f31081d0b
guillermo haas-thompson <aesthetica@me.com>
2012-09-28 00:43:18 -0400

    add page title helper
    based on railstutorial version from 3.2

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,12 @@
 module PagesHelper
+  # Returns the full title on a per-page basis.
+  def full_title(page_title)
+    base_title = "Chasing Buddha, a film by Amiel Courtin-Wilson"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
+
 end
