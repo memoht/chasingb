c56c9b174a444d535e67b67d585c81ad0413c50c
guillermo haas-thompson <guillermo@brownbox.me>
2015-01-11 11:19:56 -0500

    rails 4.2 app upgrade

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
-Chasingb::Application.routes.draw do
-  
+Rails.application.routes.draw do
   # memo: http://railscasts.com/episodes/117-semi-static-pages-revised
   %w[about amiel buy contact dashboard praise robina].each do |page|
     get page, controller: 'pages', action: page
   end
 
+  root 'pages#home'
   resources :posts, except: [:show]
   resources :quotes, except: [:show]
-  match '/press', to: 'posts#index'
-  root to: "pages#home"
+  get '/press' => 'posts#index'
 end
